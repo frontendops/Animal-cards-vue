7125b7531963f8e87ec0a78dea8411f359fffa1d
Alejandro-Sierra <alexsierra1598@gmail.com>
2019-05-18 20:45:12 -0400

    added data

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <Card :animal="animal"/>
+    <Card v-for="animal in animals" :key="animal.fact" :animal="animal"/>
   </div>
 </template>
 
@@ -14,21 +14,38 @@ export default {
   },
   data: function() {
     return {
-      animal: {
-        name: "Clownfish",
-        img:
-          "https://images.unsplash.com/photo-1544552866-d3ed42536cfd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2251&q=80",
-        fact:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, fuga. Dolores alias vel quo",
-        details: {
-          "Scientific Name": "Amphipriotine",
-          "Average length": "11 cm",
-          "Average Lifespan": "7 years",
-          Habitat: "Tropical Coral Reef"
+      animals: [
+        {
+          name: "Clownfish",
+          img:
+            "https://images.unsplash.com/photo-1544552866-d3ed42536cfd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2251&q=80",
+          fact:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, fuga. Dolores alias vel quo",
+          details: {
+            "Scientific Name": "Amphipriotine",
+            "Average length": "11 cm",
+            "Average Lifespan": "7 years",
+            Habitat: "Tropical Coral Reef"
+          },
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae maiores eaque adipisci dolor deleniti voluptate iste quia, nostrum tenetur consectetur ab tempore quaerat quo. Ratione dolorem temporibus delectus ipsa perspiciatis."
         },
-        description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae maiores eaque adipisci dolor deleniti voluptate iste quia, nostrum tenetur consectetur ab tempore quaerat quo. Ratione dolorem temporibus delectus ipsa perspiciatis."
-      }
+        {
+          name: "Dog",
+          img:
+            "https://images.unsplash.com/photo-1453227588063-bb302b62f50b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80://images.unsplash.com/photo-1544552866-d3ed42536cfd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2251&q=80",
+          fact:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, fuga. Dolores alias vel quo",
+          details: {
+            "Scientific Name": "doggy",
+            "Average length": "45 cm",
+            "Average Lifespan": "11 years",
+            Habitat: "Suburban homes"
+          },
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae maiores eaque adipisci dolor deleniti voluptate iste quia, nostrum tenetur consectetur ab tempore quaerat quo. Ratione dolorem temporibus delectus ipsa perspiciatis."
+        }
+      ]
     };
   }
 };
